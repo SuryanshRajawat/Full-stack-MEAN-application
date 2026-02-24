12507a2af4b3f0aac1b25f674ddf506428974c7f
SuryanshRajawat <suryanshrajawat.cs19@gmail.com>
2026-02-24 23:26:39 +0530

    files added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 // parse requests of content-type - application/json
 app.use(express.json());
+app.use(cors())
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -31,7 +32,8 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}`);
 });
